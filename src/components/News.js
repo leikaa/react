e594740155c8333399208f7fe5a4996ffc709282
Vyacheslav <myeve2007@yandex.ru>
2019-10-17 18:23:07 +0300

    feat: add spam filter with test - word trigger

--- src/components/News.js
+++ src/components/News.js
@@ -3,29 +3,45 @@ import PropTypes from 'prop-types';
 import { Article } from './Article';
 
 class News extends React.Component {
+    state = {
+        filteredNews: this.props.data
+    };
+
+    componentWillReceiveProps(nextProps) {
+        let nextFilteredNews = [...nextProps.data];
+
+        nextFilteredNews.forEach((item, index) => {
+            if (item.bigText.toLowerCase().indexOf('test') !== -1) {
+                item.bigText = 'СПАМ'
+            }
+        });
+
+        this.setState({ filteredNews: nextFilteredNews });
+    }
+
     renderNews = () => {
-        const { data } = this.props;
+        const { filteredNews } = this.state;
         let newsTemplate = null;
 
-        if (data.length) {
-            newsTemplate = data.map(function(item) {
+        if (filteredNews.length) {
+            newsTemplate = filteredNews.map(function(item) {
                 return <Article key={item.id} data={item}/>
             })
         } else {
             newsTemplate = <p>К сожалению новостей нет</p>
         }
 
-        return newsTemplate
+        return newsTemplate;
     };
 
     render() {
-        const { data } = this.props;
+        const { filteredNews } = this.state;
 
         return (
             <div className='news'>
                 {this.renderNews()}
                 {
-                    data.length ? <strong className={'news__count'}>Всего новостей: {data.length}</strong> : null
+                    filteredNews.length ? <strong className={'news__count'}>Всего новостей: {filteredNews.length}</strong> : null
                 }
             </div>
         )
